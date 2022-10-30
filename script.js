461e059b856c1185d1f17dde507dca4413f62611
Flixleon <65601821+Flixleon@users.noreply.github.com>
2022-10-30 09:16:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,475 @@
+const MontagFra = document.querySelector(".MoFraFach");
+const MoFra = document.querySelector(".MoFra");
+
+MontagFra.addEventListener("click", function(event) {
+    if(MoFra.style.display === 'none' || MoFra.style.display === '') {
+        allHide();
+        MoFra.style.display = 'block';
+        MoFra.style.zIndex = "1"
+    } else {
+        MoFra.style.display = 'none';
+    }
+});
+
+const MontagDeu1 = document.querySelector(".MoDeu1Fach");
+const MoDeu1 = document.querySelector(".MoDeu1");
+
+MontagDeu1.addEventListener("click", function(event) {
+    if(MoDeu1.style.display === 'none' || MoDeu1.style.display === '') {
+        allHide();
+        MoDeu1.style.display = 'block';
+        MoDeu1.style.zIndex = "1"
+    } else {
+        MoDeu1.style.display = 'none';
+    }
+});
+
+const MontagDeu2 = document.querySelector(".MoDeu2Fach");
+const MoDeu2 = document.querySelector(".MoDeu2");
+
+MontagDeu2.addEventListener("click", function(event) {
+    if(MoDeu2.style.display === 'none' || MoDeu2.style.display === '') {
+        allHide();
+        MoDeu2.style.display = 'block';
+        MoDeu2.style.zIndex = "1"
+    } else {
+        MoDeu2.style.display = 'none';
+    }
+});
+
+const MontagEng = document.querySelector(".MoEngFach");
+const MoEng = document.querySelector(".MoEng");
+
+MontagEng.addEventListener("click", function(event) {
+    if(MoEng.style.display === 'none' || MoEng.style.display === '') {
+        allHide();
+        MoEng.style.display = 'block';
+        MoEng.style.zIndex = "1"
+    } else {
+        MoEng.style.display = 'none';
+    }
+});
+
+const MontagWpf = document.querySelector(".MoWpfFach");
+const MoWpf = document.querySelector(".MoWpf");
+
+MontagWpf.addEventListener("click", function(event) {
+    if(MoWpf.style.display === 'none' || MoWpf.style.display === '') {
+        allHide();
+        MoWpf.style.display = 'block';
+        MoWpf.style.zIndex = "1"
+    } else {
+        MoWpf.style.display = 'none';
+    }
+});
+const MontagWpf2 = document.querySelector(".MoWpf2Fach");
+
+MontagWpf2.addEventListener("click", function(event) {
+    if(MoWpf.style.display === 'none' || MoWpf.style.display === '') {
+        allHide();
+        MoWpf.style.display = 'block';
+        MoWpf.style.zIndex = "1"
+    } else {
+        MoWpf.style.display = 'none';
+    }
+});
+
+const MontagSpo = document.querySelector(".MoSpoFach");
+const MoSpo = document.querySelector(".MoSpo");
+
+MontagSpo.addEventListener("click", function(event) {
+    if(MoSpo.style.display === 'none' || MoSpo.style.display === '') {
+        allHide();
+        MoSpo.style.display = 'block';
+        MoSpo.style.zIndex = "1"
+    } else {
+       MoSpo.style.display = 'none';
+    }
+});
+
+const MontagGGG = document.querySelector(".MoGGGFach");
+const MoGGG = document.querySelector(".MoGGG");
+
+MontagGGG.addEventListener("click", function(event) {
+    if(MoGGG.style.display === 'none' || MoGGG.style.display === '') {
+        allHide();
+        MoGGG.style.display = 'block';
+        MoGGG.style.zIndex = "1"
+    } else {
+        MoGGG.style.display = 'none';
+    }
+});
+
+const DienstagMat1 = document.querySelector(".DiMat1Fach");
+const DiMat1 = document.querySelector(".DiMat1");
+
+DienstagMat1.addEventListener("click", function(event) {
+    if(DiMat1.style.display === 'none' || DiMat1.style.display === '') {
+        allHide();
+        DiMat1.style.display = 'block';
+        DiMat1.style.zIndex = "1"
+    } else {
+        DiMat1.style.display = 'none';
+    }
+});
+
+const DienstagMat2 = document.querySelector(".DiMat2Fach");
+const DiMat2 = document.querySelector(".DiMat2");
+
+DienstagMat2.addEventListener("click", function(event) {
+    if(DiMat2.style.display === 'none' || DiMat2.style.display === '') {
+        allHide();
+        DiMat2.style.display = 'block';
+        DiMat2.style.zIndex = "1"
+    } else {
+        DiMat2.style.display = 'none';
+    }
+});
+
+const DienstagEng = document.querySelector(".DiEngFach");
+const DiEng = document.querySelector(".DiEng");
+
+DienstagEng.addEventListener("click", function(event) {
+    if(DiEng.style.display === 'none' || DiEng.style.display === '') {
+        allHide();
+        DiEng.style.display = 'block';
+        DiEng.style.zIndex = "1"
+    } else {
+        DiEng.style.display = 'none';
+    }
+});
+
+const DienstagGGG = document.querySelector(".DiGeoFach");
+const DiGGG = document.querySelector(".DiGGG")
+
+DienstagGGG.addEventListener("click", function(event) {
+    if(DiGGG.style.display === 'none' || DiGGG.style.display === '') {
+        allHide();
+        DiGGG.style.display = 'block';
+        DiGGG.style.zIndex = "1"
+    } else {
+        DiGGG.style.display = 'none';
+    }
+});
+
+const DienstagDeu = document.querySelector(".DiDeuFach");
+const DiDeu = document.querySelector(".DiDeu")
+
+DienstagDeu.addEventListener("click", function(event) {
+    if(DiDeu.style.display === 'none' || DiDeu.style.display === '') {
+        allHide();
+        DiDeu.style.display = 'block';
+        DiDeu.style.zIndex = "1"
+    } else {
+        DiDeu.style.display = 'none';
+    }
+});
+
+const DienstagNat = document.querySelector(".DiNatFach");
+const DiNat = document.querySelector(".DiNat")
+
+DienstagNat.addEventListener("click", function(event) {
+    if(DiNat.style.display === 'none' || DiNat.style.display === '') {
+        allHide();
+        DiNat.style.display = 'block';
+        DiNat.style.zIndex = "1"
+    } else {
+        DiNat.style.display = 'none';
+    }
+});
+const DienstagNat2 = document.querySelector(".DiNat2Fach");
+
+DienstagNat2.addEventListener("click", function(event) {
+    if(DiNat.style.display === 'none' || DiNat.style.display === '') {
+        allHide();
+        DiNat.style.display = 'block';
+        DiNat.style.zIndex = "1"
+    } else {
+        DiNat.style.display = 'none';
+    }
+});
+
+const DienstagFra = document.querySelector(".DiFraFach");
+const DiFra = document.querySelector(".DiFra")
+
+DienstagFra.addEventListener("click", function(event) {
+    if(DiFra.style.display === 'none' || DiFra.style.display === '') {
+        allHide();
+        DiFra.style.display = 'block';
+        DiFra.style.zIndex = "1"
+    } else {
+        DiFra.style.display = 'none';
+    }
+});
+
+const MittwochDeu = document.querySelector(".MiDeuFach");
+const MiDeu = document.querySelector(".MiDeu")
+
+MittwochDeu.addEventListener("click", function(event) {
+    if(MiDeu.style.display === 'none' || MiDeu.style.display === '') {
+        allHide();
+        MiDeu.style.display = 'block';
+        MiDeu.style.zIndex = "1"
+    } else {
+        MiDeu.style.display = 'none';
+    }
+});
+
+const MittwochEng = document.querySelector(".MiEngFach");
+const MiEng = document.querySelector(".MiEng")
+
+MittwochEng.addEventListener("click", function(event) {
+    if(MiEng.style.display === 'none' || MiEng.style.display === '') {
+        allHide();
+        MiEng.style.display = 'block';
+        MiEng.style.zIndex = "1"
+    } else {
+        MiEng.style.display = 'none';
+    }
+});
+
+const MittwochMat1 = document.querySelector(".MiMat1Fach");
+const MiMat1 = document.querySelector(".MiMat1")
+
+MittwochMat1.addEventListener("click", function(event) {
+    if(MiMat1.style.display === 'none' || MiMat1.style.display === '') {
+        allHide();
+        MiMat1.style.display = 'block';
+        MiMat1.style.zIndex = "1"
+    } else {
+        MiMat1.style.display = 'none';
+    }
+});
+
+const MittwochMat2 = document.querySelector(".MiMat2Fach");
+const MiMat2 = document.querySelector(".MiMat2")
+
+MittwochMat2.addEventListener("click", function(event) {
+    if(MiMat2.style.display === 'none' || MiMat2.style.display === '') {
+        allHide();
+        MiMat2.style.display = 'block';
+        MiMat2.style.zIndex = "1"
+    } else {
+        MiMat2.style.display = 'none';
+    }
+});
+
+const MittwochNat = document.querySelector(".MiNatFach");
+const MiNat = document.querySelector(".MiNat")
+
+MittwochNat.addEventListener("click", function(event) {
+    if(MiNat.style.display === 'none' || MiNat.style.display === '') {
+        allHide();
+        MiNat.style.display = 'block';
+        MiNat.style.zIndex = "1"
+    } else {
+        MiNat.style.display = 'none';
+    }
+});
+
+const DonnerstagMat = document.querySelector(".DoMatFach");
+const DoMat = document.querySelector(".DoMat")
+
+DonnerstagMat.addEventListener("click", function(event) {
+    if(DoMat.style.display === 'none' || DoMat.style.display === '') {
+        allHide();
+        DoMat.style.display = 'block';
+        DoMat.style.zIndex = "1"
+    } else {
+        DoMat.style.display = 'none';
+    }
+});
+
+const DonnerstagSa = document.querySelector(".DoSaFach");
+const DoSa = document.querySelector(".DoSa")
+
+DonnerstagSa.addEventListener("click", function(event) {
+    if(DoSa.style.display === 'none' || DoSa.style.display === '') {
+        allHide();
+        DoSa.style.display = 'block';
+        DoSa.style.zIndex = "1"
+    } else {
+        DoSa.style.display = 'none';
+    }
+});
+const DonnerstagSa2 = document.querySelector(".DoSa2Fach");
+
+DonnerstagSa2.addEventListener("click", function(event) {
+    if(DoSa.style.display === 'none' || DoSa.style.display === '') {
+        allHide();
+        DoSa.style.display = 'block';
+        DoSa.style.zIndex = "1"
+    } else {
+        DoSa.style.display = 'none';
+    }
+});
+const DonnerstagSa3 = document.querySelector(".DoSa3Fach");
+
+DonnerstagSa3.addEventListener("click", function(event) {
+    if(DoSa.style.display === 'none' || DoSa.style.display === '') {
+        allHide();
+        DoSa.style.display = 'block';
+        DoSa.style.zIndex = "1"
+    } else {
+        DoSa.style.display = 'none';
+    }
+});
+
+const DonnerstagSpo = document.querySelector(".DoSpoFach");
+const DoSpo = document.querySelector(".DoSpo")
+
+DonnerstagSpo.addEventListener("click", function(event) {
+    if(DoSpo.style.display === 'none' || DoSpo.style.display === '') {
+        allHide();
+        DoSpo.style.display = 'block';
+        DoSpo.style.zIndex = "1"
+    } else {
+        DoSpo.style.display = 'none';
+    }
+});
+const DonnerstagSpo2 = document.querySelector(".DoSpo2Fach");
+
+DonnerstagSpo2.addEventListener("click", function(event) {
+    if(DoSpo.style.display === 'none' || DoSpo.style.display === '') {
+        allHide();
+        DoSpo.style.display = 'block';
+        DoSpo.style.zIndex = "1"
+    } else {
+        DoSpo.style.display = 'none';
+    }
+});
+const DonnerstagWah = document.querySelector(".DoWahFach");
+const DoWah = document.querySelector(".DoWah")
+
+DonnerstagWah.addEventListener("click", function(event) {
+    if(DoWah.style.display === 'none' || DoWah.style.display === '') {
+        allHide();
+        DoWah.style.display = 'block';
+        DoWah.style.zIndex = "1"
+    } else {
+        DoWah.style.display = 'none';
+    }
+});
+const DonnerstagWah2 = document.querySelector(".DoWah2Fach");
+
+DonnerstagWah2.addEventListener("click", function(event) {
+    if(DoWah.style.display === 'none' || DoWah.style.display === '') {
+        allHide();
+        DoWah.style.display = 'block';
+        DoWah.style.zIndex = "1"
+    } else {
+        DoWah.style.display = 'none';
+    }
+});
+
+const FreitagInf = document.querySelector(".FrInfFach");
+const FrInf = document.querySelector(".FrInf")
+
+FreitagInf.addEventListener("click", function(event) {
+    if(FrInf.style.display === 'none' || FrInf.style.display === '') {
+        allHide();
+        FrInf.style.display = 'block';
+        FrInf.style.zIndex = "1"
+    } else {
+        FrInf.style.display = 'none';
+    }
+});
+
+const FreitagMus = document.querySelector(".FrMusFach");
+const FrMus = document.querySelector(".FrMus")
+
+FreitagMus.addEventListener("click", function(event) {
+    if(FrMus.style.display === 'none' || FrMus.style.display === '') {
+        allHide();
+        FrMus.style.display = 'block';
+        FrMus.style.zIndex = "1"
+    } else {
+        FrMus.style.display = 'none';
+    }
+});
+
+const FreitagFra = document.querySelector(".FrFraFach");
+const FrFra = document.querySelector(".FrFra")
+
+FreitagFra.addEventListener("click", function(event) {
+    if(FrFra.style.display === 'none' || FrFra.style.display === '') {
+        allHide();
+        FrFra.style.display = 'block';
+        FrFra.style.zIndex = "1"
+    } else {
+        FrFra.style.display = 'none';
+    }
+});
+
+const FreitagGGG = document.querySelector(".FrGGGFach");
+const FrGGG = document.querySelector(".FrGGG")
+
+FreitagGGG.addEventListener("click", function(event) {
+    if(FrGGG.style.display === 'none' || FrGGG.style.display === '') {
+        allHide();
+        FrGGG.style.display = 'block';
+        FrGGG.style.zIndex = "1"
+    } else {
+        FrGGG.style.display = 'none';
+    }
+});
+
+const FreitagWpf = document.querySelector(".FrWpfFach");
+const FrWpf = document.querySelector(".FrWpf")
+
+FreitagWpf.addEventListener("click", function(event) {
+    if(FrWpf.style.display === 'none' || FrWpf.style.display === '') {
+        allHide();
+        FrWpf.style.display = 'block';
+        FrWpf.style.zIndex = "1"
+    } else {
+        FrWpf.style.display = 'none';
+    }
+});
+const FreitagWpf2 = document.querySelector(".FrWpf2Fach");
+
+FreitagWpf2.addEventListener("click", function(event) {
+    if(FrWpf.style.display === 'none' || FrWpf.style.display === '') {
+        allHide();
+        FrWpf.style.display = 'block';
+        FrWpf.style.zIndex = "1"
+    } else {
+        FrWpf.style.display = 'none';
+    }
+});
+
+function allHide(){
+    MoFra.style.display = 'none';
+    MoDeu1.style.display = 'none';
+    MoDeu2.style.display = 'none';
+    MoEng.style.display = 'none';
+    MoWpf.style.display = 'none';
+    MoSpo.style.display = 'none';
+    MoGGG.style.display = 'none';
+
+
+    DiMat1.style.display = 'none';
+    DiMat2.style.display = 'none';
+    DiEng.style.display = 'none';
+    DiGGG.style.display = 'none';
+    DiNat.style.display = 'none';
+    DiFra.style.display = 'none';
+
+    MiDeu.style.display = 'none';
+    MiEng.style.display = 'none';
+    MiMat1.style.display = 'none';
+    MiMat2.style.display = 'none';
+    MiNat.style.display = 'none';
+    MiMat2.style.display = 'none';
+    
+    DoMat.style.display = 'none';
+    DoSa.style.display = 'none';
+    DoSpo.style.display = 'none';
+    DoWah.style.display = 'none';
+
+    FrInf.style.display = 'none';
+    FrMus.style.display = 'none';
+    FrFra.style.display = 'none';
+    FrGGG.style.display = 'none';
+    FrWpf.style.display = 'none';
+}
